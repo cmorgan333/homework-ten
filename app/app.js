2fd49ad5f34b0db322611578215f00c71faec1ca
cmorgan333 <78804785+cmorgan333@users.noreply.github.com>
2021-04-18 18:55:47 -0400

    model
    model

--- app/app.js
+++ app/app.js
@@ -1,11 +1,36 @@
 import * as MODEL from "../model/model.js";
 
-// this is a click listener
-
+// removing home hero image
 function init() {
+  let currentHeroImage = "home";
+  let currentNav = "home";
+
+  // this is a click listener
+
   $("nav a").click(function (event) {
     let btnId = this.id;
     let contentID = btnId + "Content";
+
+    // if/else statement for removing home hero image
+
+    if (btnId == "home") {
+      $(".hero")
+        .removeClass(`${currentHeroImage}-hero-image`)
+        .addClass(`${btnId}-hero-image`);
+      currentHeroImage = btnId;
+    } else {
+      $(".hero").removeClass(`${currentHeroImage}-hero-image`);
+      currentHeroImage = btnId;
+    }
+
+    if (btnId == "home") {
+      $("nav").removeClass(`${currentNav}-white`)
+      addClass(`${btnId}-white`);
+      currentNav = btnId;
+    } else {
+      $("nav").removeClass(`${currentNav}-white');
+      currentNav = btnId;
+    
 
     MODEL.getPageContent(contentID);
   });
