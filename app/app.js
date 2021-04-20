ae10eb4ceb7023b93e63a702849e3fa0569a1964
cmorgan333 <78804785+cmorgan333@users.noreply.github.com>
2021-04-19 21:43:54 -0400

    jquery
    jquery

--- app/app.js
+++ app/app.js
@@ -11,8 +11,6 @@ function init() {
     let btnId = this.id;
     let contentID = btnId + "Content";
 
-    console.log(btnId);
-
     // $("button").click(function () {
     //   $("nav").css("color", "black");
     // });
