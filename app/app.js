9f02fcd058c30291c8ddfd6fb9ae67a59e786367
cmorgan333 <78804785+cmorgan333@users.noreply.github.com>
2021-04-19 20:47:55 -0400

    jquery
    jquery

--- app/app.js
+++ app/app.js
@@ -11,26 +11,41 @@ function init() {
     let btnId = this.id;
     let contentID = btnId + "Content";
 
-    // if/else statement for removing home hero image
+    // $("button").click(function () {
+    //   $("nav").css("color", "black");
+    // });
+
+    // if/else statement for removing home hero image and replacing with nav only
 
     if (btnId == "home") {
+      $(".hero").css("display", "block");
       $(".hero")
         .removeClass(`${currentHeroImage}-hero-image`)
         .addClass(`${btnId}-hero-image`);
       currentHeroImage = btnId;
     } else {
       $(".hero").removeClass(`${currentHeroImage}-hero-image`);
+      $(".hero").css("display", "none");
       currentHeroImage = btnId;
     }
 
-    if (btnId == "home") {
-      $("nav").removeClass(`${currentNav}-white`)
-      addClass(`${btnId}-white`);
-      currentNav = btnId;
-    } else {
-      $("nav").removeClass(`${currentNav}-white');
-      currentNav = btnId;
-    
+    // if (btnId == "home") {
+    //   $(".white-nav")
+    //     .removeClass(`${currentNav}-white-nav`)
+    //     .addClass(`${btnId}-black-nav`);
+    // } else {
+    //   $(".white-nav").removeClass(`${currentNav}-white-nav`);
+    //   currentNav = btnId;
+    // }
+
+    // if (btnId == "home") {
+    //   $("nav").removeClass(`${currentNav}-white`)
+    //   addClass(`${btnId}-white`);
+    //   currentNav = btnId;
+    // } else {
+    //   $("nav").removeClass(`${currentNav}-white');
+    //   currentNav = btnId;
+    // }
 
     MODEL.getPageContent(contentID);
   });
